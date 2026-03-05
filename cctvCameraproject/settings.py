8c6ee2920a0988e9025120acf89da2cf1aa4817f
saurabhdhage34 <saurabhdhage20114@gmail.com>
2026-03-05 11:42:04 +0530

    Change database configuration to MySQL
    Removed SQLite configuration and set MySQL as the default database.

--- cctvCameraproject/settings.py
+++ cctvCameraproject/settings.py
@@ -63,14 +63,6 @@ WSGI_APPLICATION = 'cctvCameraproject.wsgi.application'
 # Database
 # 
 
-if os.environ.get('NGROK_LIVE'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
