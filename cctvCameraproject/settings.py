b7a0145b4cca86ca0602036d0257a8443521b7fa
saurabhdhage34 <saurabhdhage20114@gmail.com>
2026-03-05 11:43:38 +0530

    Configure database settings for different environments

--- cctvCameraproject/settings.py
+++ cctvCameraproject/settings.py
@@ -62,7 +62,14 @@ WSGI_APPLICATION = 'cctvCameraproject.wsgi.application'
 
 # Database
 # 
-
+if os.environ.get(''):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
