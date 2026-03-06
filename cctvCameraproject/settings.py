519da70ee8b25d42aeac1a157c045a06b254c093
saurabh dhage <saurabhdhage20114@gmail.com>
2026-03-06 12:26:07 +0530

    render deployment fixed

--- cctvCameraproject/settings.py
+++ cctvCameraproject/settings.py
@@ -1,148 +1,85 @@
-"""
-Django settings for cctvCameraproject project.
-"""
-
 from pathlib import Path
 import os
 
-
-
-# Build paths inside the project
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--x7qow1tugob2)p+2seog#xwq@xj3ju$qvtmgl0=q+0r@e14m+'
+SECRET_KEY = 'django-secret-key'
 
 DEBUG = False
-ALLOWED_HOSTS = ['cctv-camera-project-2.onrender.com','*']
 
+ALLOWED_HOSTS = [
+'cctv-camera-project-2.onrender.com',
+'localhost',
+'127.0.0.1'
+]
 
-
-# Application definition
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'cameraapp',
+'django.contrib.admin',
+'django.contrib.auth',
+'django.contrib.contenttypes',
+'django.contrib.sessions',
+'django.contrib.messages',
+'django.contrib.staticfiles',
+'cameraapp',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+'django.middleware.security.SecurityMiddleware',
+'whitenoise.middleware.WhiteNoiseMiddleware',
+'django.contrib.sessions.middleware.SessionMiddleware',
+'django.middleware.common.CommonMiddleware',
+'django.middleware.csrf.CsrfViewMiddleware',
+'django.contrib.auth.middleware.AuthenticationMiddleware',
+'django.contrib.messages.middleware.MessageMiddleware',
+'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'cctvCameraproject.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templest')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+{
+'BACKEND': 'django.template.backends.django.DjangoTemplates',
+'DIRS': [BASE_DIR / 'templest'],
+'APP_DIRS': True,
+'OPTIONS': {
+'context_processors': [
+'django.template.context_processors.request',
+'django.contrib.auth.context_processors.auth',
+'django.contrib.messages.context_processors.messages',
+],
+},
+},
 ]
 
 WSGI_APPLICATION = 'cctvCameraproject.wsgi.application'
 
+
+# DATABASE (Render PostgreSQL)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cctv_db_ghv2',
-        'USER': 'cctv_user',
-        'PASSWORD': 'wVL3xRSTOzQ3N4lHpgmmtdCm7iWp9TZh',
-        'HOST': 'dpg-d6l72nntskes73ej138g-a',
-        'PORT': '5432',
-        'OPTIONS': {
-            'sslmode': 'require',
-        }
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'cctv_db_ghv2',
+'USER': 'cctv_user',
+'PASSWORD': 'wVL3xRSTOzQ3N4lHpgmmtdCm7iWp9TZh',
+'HOST': 'dpg-d6l72nntskes73ej138g-a.oregon-postgres.render.com',
+'PORT': '5432',
+'OPTIONS': {
+'sslmode': 'require',
 }
-  
+}
+}
 
 
-
-
-
-
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
-]
-
-# Internationalization
-# LANGUAGE_CODE = 'en-us'
-# TIME_ZONE = 'UTC'
-# USE_I18N = True
-# USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-
-
-
-
-from pathlib import Path
-import os
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+# STATIC
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',       
-]
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-
-
-
-
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-
-# EMAIL_HOST_USER = 'saurabhdhage20114@gmail.com'
-# EMAIL_HOST_PASSWORD = 'wcqxaafxktkmwwyf'
-
-
-
-
-# import ssl
-# import certifi
-
-# ssl._create_default_https_context = ssl._create_unverified_context
-
-
-
-
-
-
-
