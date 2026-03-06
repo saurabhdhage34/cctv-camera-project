856abf2c328175882bf6223862fd86203cf0f804
saurabhdhage34 <saurabhdhage20114@gmail.com>
2026-03-06 14:20:46 +0530

    Update settings.py

--- cctvCameraproject/settings.py
+++ cctvCameraproject/settings.py
@@ -74,11 +74,14 @@ DATABASES = {
 # STATIC
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
